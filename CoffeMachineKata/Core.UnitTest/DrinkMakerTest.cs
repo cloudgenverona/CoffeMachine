ccf7774ced7ea225fd55a0f33ff8bb8e07fe3c6e
Andrea Tosato <andrea.tosato@hotmail.it>
2019-03-12 19:22:14 +0100

    Revert "Update"
    This reverts commit 473f9d3541350f8bdde28654d80b9c5c76b75083.

--- CoffeMachineKata/Core.UnitTest/DrinkMakerTest.cs
+++ CoffeMachineKata/Core.UnitTest/DrinkMakerTest.cs
@@ -6,67 +6,39 @@ namespace Core.UnitTest
     public class DrinkMakerTest
     {
         [Fact]
-        public void DrinkMakerTest_1_Tea_1_Sugar_1_Stick_Money_40()
+        public void DrinkMakerTest_1_Tea_1_Sugar_1_Stick()
         {
-            DrinkMaker d = new DrinkMaker("T:1:0",40);
+            DrinkMaker d = new DrinkMaker("T:1:0");
             Assert.Equal(DrinkType.Tea, d.DrinkType);
             Assert.Equal(1, d.Sugar);
             Assert.True(d.IsStickPresent);
         }
 
         [Fact]
-        public void DrinkMakerTest_1_Chocolate_1_Sugar_1_Stick_Money_50()
+        public void DrinkMakerTest_1_Chocolate_1_Sugar_1_Stick()
         {
-            DrinkMaker d = new DrinkMaker("H::",50);
+            DrinkMaker d = new DrinkMaker("H::");
             Assert.Equal(DrinkType.Chocolate, d.DrinkType);
             Assert.Equal(0, d.Sugar);
             Assert.False(d.IsStickPresent);
         }
 
         [Fact]
-        public void DrinkMakerTest_1_Coffe_2_Sugar_1_Stick_Money_60()
+        public void DrinkMakerTest_1_Coffe_2_Sugar_1_Stick()
         {
-            DrinkMaker d = new DrinkMaker("C:2:0",60);
+            DrinkMaker d = new DrinkMaker("C:2:0");
             Assert.Equal(DrinkType.Coffe, d.DrinkType);
             Assert.Equal(2, d.Sugar);
             Assert.True(d.IsStickPresent);
         }
-        
+
         [Fact]
         public void DrinkMakerTest_M_Message()
         {
-            DrinkMaker d = new DrinkMaker("M:message-content", 0);
+            DrinkMaker d = new DrinkMaker("M:message-content");
             Assert.Equal(DrinkType.Service, d.DrinkType);
             Assert.Equal(0, d.Sugar);
             Assert.False(d.IsStickPresent);
-            Assert.Equal("message-content", d.Message);
         }
-
-        [Fact]
-        public void DrinkMakerTest_1_Tea_1_Sugar_1_Stick_Money_20()
-        {
-            DrinkMaker d = new DrinkMaker("T:1:0",20);
-            Assert.Equal(DrinkType.Service, d.DrinkType);
-            Assert.Equal("Mancano 20 centesimi", d.Message);
-        }
-
-
-        [Fact]
-        public void DrinkMakerTest_1_Chocolate_1_Sugar_1_Stick_Money_20()
-        {
-            DrinkMaker d = new DrinkMaker("H::", 20);
-            Assert.Equal(DrinkType.Service, d.DrinkType);
-            Assert.Equal("Mancano 30 centesimi", d.Message);
-        }
-
-        [Fact]
-        public void DrinkMakerTest_1_Coffe_2_Sugar_1_Stick_Money_20()
-        {
-            DrinkMaker d = new DrinkMaker("C:2:0", 20);
-            Assert.Equal(DrinkType.Service, d.DrinkType);
-            Assert.Equal("Mancano 40 centesimi", d.Message);
-        }
-
-
     }
 }
--- CoffeMachineKata/Core/DrinkMaker.cs
+++ CoffeMachineKata/Core/DrinkMaker.cs
@@ -6,25 +6,22 @@ namespace Core
     public class DrinkMaker
     {
         private string[] _commandInstructions;
-        public DrinkMaker(string command, int money)
+        public DrinkMaker(string command)
         {
             if (command.Count(x => x == ':') < 1 && command.Count(x => x == ':') > 2)
                 throw new ArgumentException("Comando nel formato non corretto");
-            int r = 0;
+
             _commandInstructions = command.Split(":");
             switch (_commandInstructions.FirstOrDefault())
             {
                 case "T":
                     DrinkType = DrinkType.Tea;
-                    r = money - 40;
                     break;
                 case "H":
                     DrinkType = DrinkType.Chocolate;
-                    r = money - 50;
                     break;
                 case "C":
                     DrinkType = DrinkType.Coffe;
-                    r = money - 60;
                     break;
                 case "M":
                     DrinkType = DrinkType.Service;
@@ -33,22 +30,12 @@ namespace Core
             if (int.TryParse(_commandInstructions.ElementAt(1), out int sugar) && sugar > 0 && sugar <= 2)
             {
                 Sugar = sugar;
-            }
-            if (command.Count(x => x == ':') == 1)
-            {
-                Message = _commandInstructions.ElementAt(1);
-            }
-            if (r < 0)
-            {
-                Message = $"Mancano {-r} centesimi";
-                DrinkType = DrinkType.Service;
-            }
+            }   
         }
 
         public DrinkType DrinkType { get; }
         public int Sugar { get; }
         public bool IsStickPresent => Sugar > 0 && DrinkType != DrinkType.Service;
-        public string Message { get; }
     }
 
     public enum DrinkType
